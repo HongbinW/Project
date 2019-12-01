ebb9a091123baf374a61b25a627afe53cb047e8d
HongbinW <501245176@qq.com>
2019-12-01 08:12:04 -0500

    V2.0

--- mmall/src/main/java/com/mmall/task/CloseOrderTask.java
+++ mmall/src/main/java/com/mmall/task/CloseOrderTask.java
@@ -21,7 +21,7 @@ public class CloseOrderTask {
 //    @Scheduled(cron = "0 */1 * * * ?")  //每min执行一次
     public void closeOrderTaskV1(){
         log.info("关闭订单定时任务启动");
-        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2");
+        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
 //        iOrderService.closeOrder(hour);
         log.info("关闭订单定时任务结束");
     }
@@ -29,7 +29,7 @@ public class CloseOrderTask {
     @Scheduled(cron = "0 */1 * * * ?")  //每min执行一次
     public void closeOrderTaskV2() {
         log.info("关闭订单定时任务启动");
-        long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout"),5000);
+        long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout","5000"));
         Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,String.valueOf(System.currentTimeMillis() + lockTimeout));
         if (setnxResult != null && setnxResult.intValue() == 1){
             //如果返回值是1，代表设置成功，获取锁
@@ -43,7 +43,7 @@ public class CloseOrderTask {
     private void closeOrder(String lockName){
         RedisShardedPoolUtil.expire(lockName,50);//有效期50秒，防止死锁
         log.info("获取{},ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
-        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2");
+        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
         iOrderService.closeOrder(hour);
         RedisShardedPoolUtil.del(lockName);//及时释放锁
         log.info("释放{},ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
